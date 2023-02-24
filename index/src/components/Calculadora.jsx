ee9dfe2c941a42ebe128545d0cba8762f5111d36
lucasmoraist <luksmnt1101@gmail.com>
2023-02-24 14:18:43 -0300

    Primeiro Layout finalizado

--- index/src/components/Calculadora.jsx
+++ index/src/components/Calculadora.jsx
@@ -1,7 +1,57 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const DivCalc = styled.div`
+    button{
+        border:none;
+        padding: 8px;
+        border-radius: 30px;
+        margin-top: 10px;
+        background-color: #836FFF;
+        color: white;
+    }
+
+    button:hover{
+        box-shadow: 10px 10px 0px #ff96ad;
+        top: -5px;
+        left:-5px;
+        transition: 0.5s;
+        cursor: pointer;
+    }
+
+    label{
+        display: block;
+        margin-top: 15px;
+        font-weight: bold;
+    }
+    #container{
+        background-color: #ADD8E6;
+        padding-left:40%;
+        padding-top: 17%;
+        padding-bottom: 13%;
+    }
+
+    input{
+        background-color: #ADD8E6;
+        border:none;
+        border-bottom: solid 2px black;
+        border-radius: 5px;
+        font-weight: bold;
+        
+    }
+    
+    input[type=number]::-webkit-inner-spin-button { 
+    -webkit-appearance: none;
+        
+    }
+
+    #container div{
+        margin-bottom: 30px;
+    }
+    
+`
 
 export default function Calculadora() {
-
     const [peso, setPeso] = useState();
     const [altura, setAltura] = useState();
     const [resultado, setResultado] = useState();
@@ -14,28 +64,28 @@ export default function Calculadora() {
     };
 
     return (
-        <div>
-            <div>
-                <label htmlFor="idAltura">Altura</label>
-                <input
-                    type="number"
-                    placeholder="ex: 170"
-                    onChange={(e) => setAltura(e.target.value)}
-                    value={altura}
-                />
-            </div>
+        <DivCalc>
+            <div id='container'>
+                <div>
+                    <input
+                        type="number"
+                        placeholder='Altura'
+                        onChange={(e) => setAltura(e.target.value)}
+                        value={altura}
+                    />
+                </div>
 
-            <div>
-                <label htmlFor="idPeso">Peso</label>
-                <input
-                    type="number"
-                    placeholder="ex: 60"
-                    onChange={(e) => setPeso(e.target.value)}
-                    value={peso}
-                />
+                <div>
+                    <input
+                        type="number"
+                        placeholder='Peso'
+                        onChange={(e) => setPeso(e.target.value)}
+                        value={peso}
+                    />
+                </div>
+                <button onClick={calcularIMC}>Calcular</button>
+                <h2>Seu IMC corporal: {resultado}</h2>
             </div>
-            <button onClick={calcularIMC}>Calcular</button>
-            <h2>Seu IMC corporal: {resultado}</h2>
-        </div>
+        </DivCalc>
     )
 }
